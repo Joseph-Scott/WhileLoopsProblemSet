cac372bb5b44e414ffab665ff5afc60e175227e0
Joseph Scott <jbs198585@gmail.com>
2019-06-21 06:16:57 -0400

    my solutions

---
+++ whileLoops.js
@@ -0,0 +1,43 @@
+//Problem 1
+
+var num = -10;
+
+while (num <= 19) {
+	console.log(num);
+	num++;
+}
+
+//Problem 2
+
+var num = 10;
+
+while (num <= 40) {
+	if(num % 2 === 0) {
+		console.log(num);
+	}
+	num++;
+}
+
+//Problem 3
+
+var num = 300;
+
+while (num <= 333) {
+	if(num % 2 !=== 0) {
+		console.log(num);
+	}
+	num++;
+}
+
+//Problem 4
+
+var num = 5;
+while (num <=50) {
+	if(num % 5 === 0) {
+		console.log(num);
+	}
+	else if (num % 3 === 0) {
+		console.log(num);
+	}
+	num++;
+}
