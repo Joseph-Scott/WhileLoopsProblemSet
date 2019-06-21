b5eddd7d66e167f8996b58b180a5d3d9f3bfc493
Joseph Scott <jbs198585@gmail.com>
2019-06-21 06:44:45 -0400

    solution

--- whileLoops.js
+++ whileLoops.js
@@ -1,4 +1,4 @@
-//Problem 1
+//Problem 1 - Print all numbers between -10 and 19.
 
 var num = -10;
 
@@ -7,7 +7,7 @@ while (num <= 19) {
 	num++;
 }
 
-//Problem 2
+//Problem 2 - Print all even numbers between 10 and 40.
 
 var num = 10;
 
@@ -15,29 +15,26 @@ while (num <= 40) {
 	if(num % 2 === 0) {
 		console.log(num);
 	}
-	num++;
+	num+=1;
 }
 
-//Problem 3
+//Problem 3 - Print all odd numbers between 300 and 333.
 
 var num = 300;
 
 while (num <= 333) {
-	if(num % 2 !=== 0) {
+	if(num % 2 !== 0) {
 		console.log(num);
 	}
-	num++;
+	num+=1;
 }
 
-//Problem 4
+//Problem 4 - Print all numbers divisible by 5 AND 3 between 5 and 50.
 
 var num = 5;
 while (num <=50) {
-	if(num % 5 === 0) {
+	if(num % 5 === 0 && num % 3 === 0) {
 		console.log(num);
 	}
-	else if (num % 3 === 0) {
-		console.log(num);
-	}
-	num++;
+	num+=1;
 }
